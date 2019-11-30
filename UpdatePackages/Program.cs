1d94592f945f2cc5990a610a757805b35ea0ad1e
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-11-30 02:09:14 +0000

    Used built in nuget rather than shelling out to nuget.exe

--- UpdatePackages/Program.cs
+++ UpdatePackages/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Xml;
 using Microsoft.Extensions.Configuration;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
 
 namespace UpdatePackages
@@ -13,12 +20,19 @@ namespace UpdatePackages
         private const int SUCCESS = 0;
         private const int ERROR = 1;
 
-        private static int Main(string[] args)
+        private const bool INCLUDE_UNLISTED_PACKAGES = false;
+
+        private static readonly SearchFilter SearchFilter =
+            new SearchFilter(includePrerelease: false, SearchFilterType.IsLatestVersion) {IncludeDelisted = INCLUDE_UNLISTED_PACKAGES, OrderBy = SearchOrderBy.Id};
+
+        private static readonly ILogger NugetLogger = new NullLogger();
+
+        private static async Task<int> Main(string[] args)
         {
             try
             {
                 IConfigurationRoot configuration = new ConfigurationBuilder()
-                    .AddCommandLine(args, new Dictionary<string, string> {{@"-prefix", @"prefix"}, {@"-version", @"version"}, {@"-folder", @"folder"}})
+                    .AddCommandLine(args, new Dictionary<string, string> {{@"-prefix", @"prefix"}, {@"-version", @"version"}, {@"-folder", @"folder"}, {@"-source", @"source"}})
                     .Build();
 
                 Dictionary<string, string> packages = new Dictionary<string, string>();
@@ -29,11 +43,23 @@ namespace UpdatePackages
 
                 string version = configuration.GetValue<string>(key: @"Version");
 
+                string source = configuration.GetValue<string>(key: @"source");
+
+                PackageSourceProvider packageSourceProvider = new PackageSourceProvider(Settings.LoadDefaultSettings(Environment.CurrentDirectory));
+
+                List<PackageSource> sources = packageSourceProvider.LoadPackageSources()
+                    .ToList();
+
+                if (!string.IsNullOrEmpty(source))
+                {
+                    sources.Add(new PackageSource(name: "Custom", source: source, isEnabled: true, isPersistable: true, isOfficial: true));
+                }
+
                 bool fromNuget = false;
 
                 if (string.IsNullOrWhiteSpace(version))
                 {
-                    FindPackages(prefix, packages);
+                    await FindPackages(sources, prefix, packages, CancellationToken.None);
                     fromNuget = true;
                 }
                 else
@@ -58,12 +84,10 @@ namespace UpdatePackages
 
                     return SUCCESS;
                 }
-                else
-                {
-                    Console.WriteLine(value: "No updates made.");
 
-                    return ERROR;
-                }
+                Console.WriteLine(value: "No updates made.");
+
+                return ERROR;
             }
             catch (Exception exception)
             {
@@ -73,34 +97,35 @@ namespace UpdatePackages
             }
         }
 
-        private static void FindPackages(string prefix, Dictionary<string, string> packages)
+        private static async Task FindPackages(List<PackageSource> sources, string prefix, Dictionary<string, string> packages, CancellationToken cancellationToken)
         {
-            Console.WriteLine(value: "Enumerating matching packages...");
-
-            ProcessStartInfo psi = new ProcessStartInfo(fileName: "nuget.exe", "list " + prefix) {RedirectStandardOutput = true, CreateNoWindow = true};
-
-            using (Process p = Process.Start(psi))
+            async Task LoadPackagesFromSource(PackageSource packageSource, ConcurrentDictionary<string, string> concurrentDictionary)
             {
-                if (p == null)
+                SourceRepository sourceRepository = new SourceRepository(packageSource, new List<Lazy<INuGetResourceProvider>>(Repository.Provider.GetCoreV3()));
+
+                PackageSearchResource searcher = await sourceRepository.GetResourceAsync<PackageSearchResource>(cancellationToken);
+                IEnumerable<IPackageSearchMetadata> result = await searcher.SearchAsync(prefix, SearchFilter, log: NugetLogger, cancellationToken: cancellationToken, skip: 0, take: int.MaxValue);
+
+                foreach (IPackageSearchMetadata entry in result)
                 {
-                    throw new NotSupportedException($"ERROR: Could not execute {psi.FileName} {psi.Arguments}");
-                }
+                    PackageVersion packageVersion = new PackageVersion(entry.Identity.Id, entry.Identity.Version.ToString());
 
-                StreamReader s = p.StandardOutput;
-
-                while (!s.EndOfStream)
-                {
-                    string? line = p.StandardOutput.ReadLine();
-
-                    PackageVersion? packageVersion = ExtractPackageVersion(line);
-
-                    if (packageVersion != null && Matches(prefix, packageVersion) && !IsBannedPackage(packageVersion))
+                    if (Matches(prefix, packageVersion) && !IsBannedPackage(packageVersion))
                     {
-                        packages.TryAdd(packageVersion.PackageId, packageVersion.Version);
+                        concurrentDictionary.TryAdd(packageVersion.PackageId, packageVersion.Version);
                     }
                 }
+            }
 
-                p.WaitForExit();
+            Console.WriteLine(value: "Enumerating matching packages...");
+
+            ConcurrentDictionary<string, string> found = new ConcurrentDictionary<string, string>();
+
+            await Task.WhenAll(sources.Select(selector: source => LoadPackagesFromSource(source, found)));
+
+            foreach (KeyValuePair<string, string> item in found)
+            {
+                packages.TryAdd(item.Key, item.Value);
             }
         }
 
@@ -112,24 +137,6 @@ namespace UpdatePackages
         private static bool IsBannedPackage(PackageVersion packageVersion)
         {
             return packageVersion.Version.Contains(value: "+", StringComparison.Ordinal) || StringComparer.InvariantCultureIgnoreCase.Equals(packageVersion.PackageId, y: "Nuget.Version");
-        }
-
-        private static PackageVersion? ExtractPackageVersion(string? line)
-        {
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                return null;
-            }
-
-            string[] pv = line.Trim()
-                .Split(separator: ' ');
-
-            if (pv.Length != 2)
-            {
-                return null;
-            }
-
-            return new PackageVersion(pv[0], pv[1]);
         }
 
         private static int UpdateProject(string project, Dictionary<string, string> packages, bool fromNuget)
