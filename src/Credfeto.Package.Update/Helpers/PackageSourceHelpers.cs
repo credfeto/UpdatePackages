5bda7896342a0172601a8e7d84dbe70b98e822d3
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-09-29 00:00:55 +0100

    Switched to newer NuGet.Protocol package

--- src/Credfeto.Package.Update/Helpers/PackageSourceHelpers.cs
+++ src/Credfeto.Package.Update/Helpers/PackageSourceHelpers.cs
@@ -6,7 +6,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Configuration;
-using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
 
