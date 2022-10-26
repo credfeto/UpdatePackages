3e0eceb33f10df91aadd0be02bbfcf89da7aa107
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-10-26 22:40:53 +0100

    Started splitting out updating into testable components

---
+++ src/Credfeto.Package/PackageUpdaterSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Credfeto.Package.Update.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Credfeto.Package;
+
+/// <summary>
+///     Configures the ethereum abi services.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class PackageUpdaterSetup
+{
+    /// <summary>
+    ///     Configures the ethereum client services.
+    /// </summary>
+    /// <param name="services">The services to add things to.</param>
+    public static IServiceCollection AddAbiTypeFactory(this IServiceCollection services)
+    {
+        return services.AddSingleton<IProjectLoader, ProjectLoader>();
+    }
+}
