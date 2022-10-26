8e7b00308b305322810c544abbdeb301d978fba6
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-10-26 23:19:50 +0100

    Started splitting out updating into testable components

--- src/Credfeto.Package/Services/ProjectLoader.cs
+++ src/Credfeto.Package/Services/ProjectLoader.cs
@@ -1,27 +1,37 @@
 using System;
 using System.Xml;
 using Microsoft.Extensions.Logging;
+using NonBlocking;
 
 namespace Credfeto.Package.Services;
 
 public sealed class ProjectLoader : IProjectLoader
 {
+    private readonly ConcurrentDictionary<string, IProject> _loadedProjects;
     private readonly ILogger<ProjectLoader> _logger;
 
     public ProjectLoader(ILogger<ProjectLoader> logger)
     {
         this._logger = logger;
+        this._loadedProjects = new(StringComparer.Ordinal);
     }
 
     public IProject? Load(string path)
     {
+        if (this._loadedProjects.TryGetValue(key: path, out IProject? project))
+        {
+            return project;
+        }
+
         try
         {
             XmlDocument doc = new();
 
             doc.Load(path);
 
-            return new Project(doc);
+            project = new Project(fileName: path, doc: doc);
+
+            return this._loadedProjects.GetOrAdd(key: path, value: project);
         }
         catch (Exception exception)
         {
