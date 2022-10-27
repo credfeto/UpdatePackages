42ae29cffe4057798cab9eb71327125776a4c78d
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-10-27 01:10:09 +0100

    Started splitting out updating into testable components

--- src/Credfeto.Package/Services/PackageRegistry.cs
+++ src/Credfeto.Package/Services/PackageRegistry.cs
@@ -1,14 +1,100 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using NonBlocking;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
 
 namespace Credfeto.Package.Services;
 
 public sealed class PackageRegistry : IPackageRegistry
 {
-    public Task<IReadOnlyList<PackageVersion>> FindPackagesAsync(CancellationToken cancellationToken)
+    private const bool INCLUDE_UNLISTED_PACKAGES = false;
+
+    private static readonly SearchFilter SearchFilter =
+        new(includePrerelease: false, filter: SearchFilterType.IsLatestVersion) { IncludeDelisted = INCLUDE_UNLISTED_PACKAGES, OrderBy = SearchOrderBy.Id };
+
+    private static readonly ILogger NugetLogger = new NullLogger();
+
+    public async Task<IReadOnlyList<PackageVersion>> FindPackagesAsync(IReadOnlyList<string> packageIds, IReadOnlyList<string> packageSources, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        IReadOnlyList<PackageSource> sources = DefinePackageSources(packageSources);
+
+        ConcurrentDictionary<string, NuGetVersion> packages = new(StringComparer.OrdinalIgnoreCase);
+
+        foreach (string packageId in packageIds)
+        {
+            await FindPackageInSourcesAsync(sources: sources, packageId: packageId, packages: packages, cancellationToken: cancellationToken);
+        }
+
+        return packages.Select(p => new PackageVersion(packageId: p.Key, version: p.Value))
+                       .ToArray();
+    }
+
+    private static IReadOnlyList<PackageSource> DefinePackageSources(IReadOnlyList<string> sources)
+    {
+        PackageSourceProvider packageSourceProvider = new(Settings.LoadDefaultSettings(Environment.CurrentDirectory));
+
+        List<PackageSource> packageSources = packageSourceProvider.LoadPackageSources()
+                                                                  .ToList();
+
+        int sourceId = 0;
+
+        foreach (string source in sources)
+        {
+            ++sourceId;
+            packageSources.Add(new(name: $"Custom{sourceId}", source: source, isEnabled: true, isPersistable: true, isOfficial: true));
+        }
+
+        return packageSources;
+    }
+
+    private static async Task LoadPackagesFromSourceAsync(PackageSource packageSource, string packageId, ConcurrentDictionary<string, NuGetVersion> found, CancellationToken cancellationToken)
+    {
+        SourceRepository sourceRepository = new(source: packageSource, new List<Lazy<INuGetResourceProvider>>(Repository.Provider.GetCoreV3()));
+
+        PackageSearchResource searcher = await sourceRepository.GetResourceAsync<PackageSearchResource>(cancellationToken);
+        IEnumerable<IPackageSearchMetadata> result = await searcher.SearchAsync(searchTerm: packageId,
+                                                                                filters: SearchFilter,
+                                                                                log: NugetLogger,
+                                                                                cancellationToken: cancellationToken,
+                                                                                skip: 0,
+                                                                                take: int.MaxValue);
+
+        foreach (PackageVersion packageVersion in result.Select(entry => entry.Identity)
+                                                        .Select(identity => new PackageVersion(packageId: identity.Id, version: identity.Version)))
+        {
+            if (StringComparer.InvariantCultureIgnoreCase.Equals(x: packageId, y: packageVersion.PackageId) && !IsBannedPackage(packageVersion))
+            {
+                found.TryAdd(key: packageVersion.PackageId, value: packageVersion.Version);
+            }
+        }
+    }
+
+    private static bool IsBannedPackage(PackageVersion packageVersion)
+    {
+        return packageVersion.Version.ToString()
+                             .Contains(value: '+', comparisonType: StringComparison.Ordinal);
+    }
+
+    private static async Task FindPackageInSourcesAsync(IReadOnlyList<PackageSource> sources,
+                                                        string packageId,
+                                                        ConcurrentDictionary<string, NuGetVersion> packages,
+                                                        CancellationToken cancellationToken)
+    {
+        Console.WriteLine(value: $"Enumerating matching package versions for {packageId}...");
+
+        ConcurrentDictionary<string, NuGetVersion> found = new(StringComparer.Ordinal);
+
+        await Task.WhenAll(sources.Select(selector: source => LoadPackagesFromSourceAsync(packageSource: source, packageId: packageId, found: found, cancellationToken: cancellationToken)));
+
+        foreach ((string key, NuGetVersion value) in found)
+        {
+            packages.TryAdd(key: key, value: value);
+        }
     }
 }
