4706630a26e320b12641433364a2c291b1a94c0a
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-10-30 19:43:39 +0000

    Added DI tests

---
+++ src/Credfeto.Package.Test/DependencyInjectionTests.cs
@@ -0,0 +1,37 @@
+﻿using FunFair.Test.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Credfeto.Package.Test;
+
+public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
+{
+    public DependencyInjectionTests(ITestOutputHelper output)
+        : base(output: output, dependencyInjectionRegistration: Configure)
+    {
+    }
+
+    [Fact]
+    public void ProjectLoaderMustBeRegistered()
+    {
+        this.RequireService<IProjectLoader>();
+    }
+
+    [Fact]
+    public void PackageUpdaterMustBeRegistered()
+    {
+        this.RequireService<IPackageUpdater>();
+    }
+
+    [Fact]
+    public void PackageRegistryMustBeRegistered()
+    {
+        this.RequireService<IPackageRegistry>();
+    }
+
+    private static IServiceCollection Configure(IServiceCollection services)
+    {
+        return services.AddPackageUpdater();
+    }
+}
