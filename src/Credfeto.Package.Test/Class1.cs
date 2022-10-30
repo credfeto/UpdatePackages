7b5329ed23d9c5fc24df236581c1bcd9e41a4023
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-10-30 19:38:05 +0000

    Started adding tests

---
+++ src/Credfeto.Package.Test/Class1.cs
@@ -0,0 +1,13 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.Package.Test;
+
+public sealed class Class1 : TestBase
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(condition: true, userMessage: "banana");
+    }
+}
