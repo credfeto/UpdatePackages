4f4926e8721f72aa57f8e1829c9ec0ed03870001
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-11-15 10:25:52 +0000

    Fixed code analysis issues

--- src/Credfeto.Package/PackageUpdaterSetup.cs
+++ src/Credfeto.Package/PackageUpdaterSetup.cs
@@ -1,10 +1,8 @@
-using System.Diagnostics.CodeAnalysis;
 using Credfeto.Package.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Credfeto.Package;
 
-[ExcludeFromCodeCoverage]
 public static class PackageUpdaterSetup
 {
     public static IServiceCollection AddPackageUpdater(this IServiceCollection services)
