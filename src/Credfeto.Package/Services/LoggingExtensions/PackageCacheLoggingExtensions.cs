0bb462dabe5529e881a82eee5d71df0e06f61170
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 11:15:57 +0000

    Cleanup: PackageCacheLoggingExtensions.cs

--- src/Credfeto.Package/Services/LoggingExtensions/PackageCacheLoggingExtensions.cs
+++ src/Credfeto.Package/Services/LoggingExtensions/PackageCacheLoggingExtensions.cs
@@ -5,25 +5,13 @@ namespace Credfeto.Package.Services.LoggingExtensions;
 
 internal static partial class PackageCacheLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 0,
-        Level = LogLevel.Information,
-        Message = "Loading cache from {fileName}"
-    )]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Information, Message = "Loading cache from {fileName}")]
     public static partial void LoadingCache(this ILogger<PackageCache> logger, string fileName);
 
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Information,
-        Message = "Saving cache to {fileName}"
-    )]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Saving cache to {fileName}")]
     public static partial void SavingCache(this ILogger<PackageCache> logger, string fileName);
 
-    [LoggerMessage(
-        EventId = 2,
-        Level = LogLevel.Information,
-        Message = "Loaded {packageId} {version} from cache"
-    )]
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "Loaded {packageId} {version} from cache")]
     public static partial void LoadedPackageVersionFromCache(
         this ILogger<PackageCache> logger,
         string packageId,
@@ -42,11 +30,7 @@ internal static partial class PackageCacheLoggingExtensions
         NuGetVersion version
     );
 
-    [LoggerMessage(
-        EventId = 4,
-        Level = LogLevel.Information,
-        Message = "Adding cache of {packageId} at {version}"
-    )]
+    [LoggerMessage(EventId = 4, Level = LogLevel.Information, Message = "Adding cache of {packageId} at {version}")]
     public static partial void AddingPackageToCache(
         this ILogger<PackageCache> logger,
         string packageId,
