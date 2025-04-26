d7f9766e57c1c2862932872eed36b4deca5f3487
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 11:19:05 +0000

    Cleanup: ProjectLoaderLoggingExtensions.cs

--- src/Credfeto.Package/Services/LoggingExtensions/ProjectLoaderLoggingExtensions.cs
+++ src/Credfeto.Package/Services/LoggingExtensions/ProjectLoaderLoggingExtensions.cs
@@ -5,11 +5,7 @@ namespace Credfeto.Package.Services.LoggingExtensions;
 
 internal static partial class ProjectLoaderLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 0,
-        Level = LogLevel.Error,
-        Message = "Failed to load {fileName}: {message}"
-    )]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Failed to load {fileName}: {message}")]
     public static partial void FailedToLoad(
         this ILogger<ProjectLoader> logger,
         string fileName,
