df305297cabf8ca9b0b1be670b3c2f5d63dd1a8f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 11:18:07 +0000

    Cleanup: PackageUpdaterLoggingExtensions.cs

--- src/Credfeto.Package/Services/LoggingExtensions/PackageUpdaterLoggingExtensions.cs
+++ src/Credfeto.Package/Services/LoggingExtensions/PackageUpdaterLoggingExtensions.cs
@@ -5,25 +5,13 @@ namespace Credfeto.Package.Services.LoggingExtensions;
 
 internal static partial class PackageUpdaterLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 0,
-        Level = LogLevel.Information,
-        Message = "No matching packages installed"
-    )]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Information, Message = "No matching packages installed")]
     public static partial void NoMatchingPackagesInstalled(this ILogger<PackageUpdater> logger);
 
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Information,
-        Message = "No matching packages found in event source"
-    )]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "No matching packages found in event source")]
     public static partial void NoMatchingPackagesInEventSource(this ILogger<PackageUpdater> logger);
 
-    [LoggerMessage(
-        EventId = 2,
-        Level = LogLevel.Information,
-        Message = "All installed packages are up to date"
-    )]
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "All installed packages are up to date")]
     public static partial void AllInstalledPackagesAreUpToDate(this ILogger<PackageUpdater> logger);
 
     [LoggerMessage(EventId = 3, Level = LogLevel.Information, Message = "Saving {fileName}")]
